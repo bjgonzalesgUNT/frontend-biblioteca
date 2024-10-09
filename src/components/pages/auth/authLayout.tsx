e9d20bba26c27f47a37e1f93a41d43ca81f46a80
DAVID SANCHEZ <72284986+David-Sanchez-Sangay@users.noreply.github.com>
2024-10-09 13:46:57 -0500

    second login update

--- src/components/pages/auth/authLayout.tsx
+++ src/components/pages/auth/authLayout.tsx
@@ -7,19 +7,10 @@ interface Props {
 
 export const AuthLayoutWrapper = ({ children }: Props) => {
   return (
-    <div className="relative flex h-screen">
-      <div className="absolute inset-0 z-0 overflow-hidden">
-        <Image
-          className="object-cover w-full h-full filter blur-sm"
-          src="https://hips.hearstapps.com/hmg-prod/images/old-books-in-a-library-big-file-royalty-free-image-1666591048.jpg"
-          alt="Background"
-          layout="fill"
-          objectFit="cover"
-        />
-        <div className="absolute inset-0 bg-black opacity-80"></div> {/* Capa oscura opcional */}
-      </div>
-      <div className="flex flex-1 flex-col items-center justify-center p-2 z-10">
-        <div className="p-2 rounded-lg shadow-lg ">
+    <div className="relative h-screen">
+      <div className="absolute inset-0 z-0 bg-[url('https://hips.hearstapps.com/hmg-prod/images/old-books-in-a-library-big-file-royalty-free-image-1666591048.jpg')] bg-cover filter blur-sm"></div>
+      <div className="relative flex flex-1 flex-col items-center justify-center p-2 z-10">
+        <div className="p-4 rounded-lg">
           {children}
         </div>
       </div>
