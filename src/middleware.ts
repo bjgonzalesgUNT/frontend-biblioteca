87d9576ce7d9fa94653827e8f4f8b1d75835d76f
Joseph Gutierrez <t022700120@unitru.edu.pe>
2024-10-24 07:59:47 -0500

    Comentado las rutas protegias

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,27 +2,27 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "./lib";
 
 export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+  // const { pathname } = request.nextUrl;
 
-  const userAuth = await getSession();
+  // const userAuth = await getSession();
 
-  if (!userAuth && pathname.includes("/dashboard")) {
-    return NextResponse.redirect(new URL("/login", request.nextUrl));
-  }
+  // if (!userAuth && pathname.includes("/dashboard")) {
+  //   return NextResponse.redirect(new URL("/login", request.nextUrl));
+  // }
 
-  if (userAuth && pathname === "/login") {
-    if (userAuth.user.role === "admin")
-      return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
-    if (userAuth.user.role === "user")
-      return NextResponse.redirect(new URL("/", request.nextUrl));
-  }
+  // if (userAuth && pathname === "/login") {
+  //   if (userAuth.user.role === "admin")
+  //     return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
+  //   if (userAuth.user.role === "user")
+  //     return NextResponse.redirect(new URL("/", request.nextUrl));
+  // }
 
-  if (
-    userAuth &&
-    pathname.includes("/dashboard") &&
-    userAuth.user.role === "user"
-  )
-    return NextResponse.redirect(new URL("/", request.nextUrl));
+  // if (
+  //   userAuth &&
+  //   pathname.includes("/dashboard") &&
+  //   userAuth.user.role === "user"
+  // )
+  //   return NextResponse.redirect(new URL("/", request.nextUrl));
 
   return NextResponse.next();
 }
