7dc70125d220852a473328525af3d30a51867262
Joseph Gutierrez <t022700120@unitru.edu.pe>
2024-12-15 17:35:05 -0500

    Agregar componente ViewBook y mejorar la carga de la página de libros con Suspense

--- src/app/(main)/books/page.tsx
+++ src/app/(main)/books/page.tsx
@@ -1,8 +1,13 @@
-import { NextPage } from "next";
 import { BookWrapper } from "@/components/pages/books/BookWrapper";
+import { NextPage } from "next";
+import { Suspense } from "react";
 
 const BooksPage: NextPage = () => {
-  return <BookWrapper />;
+  return (
+    <Suspense fallback={<div>Cargando...</div>}>
+      <BookWrapper />
+    </Suspense>
+  );
 };
 
 export default BooksPage;
